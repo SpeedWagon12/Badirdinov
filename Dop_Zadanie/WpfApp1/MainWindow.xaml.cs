c1eaa83048610ba5a78e876bd5a0342989f74485
SpeedWagon12 <151126812+SpeedWagon12@users.noreply.github.com>
2023-11-23 17:33:39 +0300

    Add files via upload

---
+++ Dop_Zadanie/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(!double.TryParse(NumberA.Text, out double a1))
+            { MessageBox.Show("Введите число!!!"); }
+
+            if (!double.TryParse(NumberA2.Text, out double a2))
+            { MessageBox.Show("Введите число!!!"); }
+
+            if (!double.TryParse(NumberB.Text, out double b1))
+            { MessageBox.Show("Введите число!!!"); }
+
+            if (!double.TryParse(NumberB2.Text, out double b2))
+            { MessageBox.Show("Введите число!!!"); }
+
+            if (!double.TryParse(NumberC.Text, out double c1))
+            { MessageBox.Show("Введите число!!!"); }
+
+            if (!double.TryParse(NumberC2.Text, out double c2))
+            { MessageBox.Show("Введите число!!!"); }
+
+            double d, x, y;
+            d = a1 * b2 - a2 * b1;
+            x = (c1 * b2 - c2 * b1) / d;
+            y = (a1 * c2 - a2 * c1) / d;
+
+            Anwer.Text += $"x = {x} \n y = {y}";
+
+
+        }
+    }
+}
---
+++ Zadanie2/Zadanie2/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadanie2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!double.TryParse(Box1.Text, out double L))
+            { MessageBox.Show("Поле только для чисел"); }
+            double p = 3.14, R, S;
+            R = L / p * 2;
+            S = p * Math.Pow(R, 2);
+            Block1.Text += "Радиус равен = " + R + "\n";
+            Block1.Text += "Площадь круга  равна = " + S ;
+
+        }
+    }
+}
